6fc46844daf6fb716ad150f8e18fcc42edfa9786
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2021-08-19 13:57:04 +0300

    added second folder. + function

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ let path = require('path');
 let util = require('util');
 
 let firstFolder = path.join(__dirname, 'girls');
+let secondFolder = path.join(__dirname, 'boys');
+
 let boysFolder = path.join(__dirname, 'boys')
 let girlsFolder = path.join(__dirname, 'girls')
 
-// const readFilePromise = util.promisify(fs.readFile);
+
 const renameFSPromise = util.promisify(fs.rename)
 
 
@@ -14,50 +16,56 @@ console.log(__dirname);
 console.log(__filename);
 console.log(firstFolder)
 
-// let sortBoysGirls = function () {
-//
-// }
+let sortBoysGirls = function (aimFolder) {
 
-fs.readdir(firstFolder, (err, files) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+    fs.readdir(aimFolder, (err, files) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
 
-    console.log(files)
+        console.log(files)
 
-    files.forEach((file) => {
-        console.log(file);
-        let filePath = path.join(firstFolder, file);
-        console.log(filePath);
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
+        files.forEach((file) => {
+            console.log(file);
+            let filePath = path.join(aimFolder, file);
+            console.log(filePath);
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
 
-            console.log(data.toString());
-            let dataText =  data.toString();
-            if (dataText.indexOf('female') === -1) {
-                console.log('this is boy')
-                console.log('current file loc = ' + filePath)
-                let newDest = path.join(boysFolder, file)
-                console.log('dest file loc = ' + newDest)
-                renameFSPromise( filePath, newDest).catch( reason =>    {
-                    console.log(reason)
+                console.log(data.toString());
+                let dataText =  data.toString();
+                if (dataText.indexOf('female') === -1) {
+                    console.log('this is boy')
+                    console.log('current file loc = ' + filePath)
+                    let newDest = path.join(boysFolder, file)
+                    console.log('dest file loc = ' + newDest)
+                    renameFSPromise( filePath, newDest).catch( reason =>    {
+                        console.log(reason)
                     } )
                 } else {
-                console.log('this is girl')
-                let newDest = path.join(girlsFolder, file)
-                console.log('dest file loc = ' + newDest)
-                renameFSPromise( filePath, newDest).catch( reason =>    {
-                    console.log(reason)
-                } )
-            }
+                    console.log('this is girl')
+                    let newDest = path.join(girlsFolder, file)
+                    console.log('dest file loc = ' + newDest)
+                    renameFSPromise( filePath, newDest).catch( reason =>    {
+                        console.log(reason)
+                    } )
+                }
 
-        });
+            });
+        })
     })
-})
+
+
+}
+
+sortBoysGirls(firstFolder);
+sortBoysGirls(secondFolder);
+
+
 
 console.log('dirname = '+ __dirname);
 console.log('filename = '+ __filename);
