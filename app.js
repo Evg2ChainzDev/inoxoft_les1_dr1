9cbab2aa51a4329f9d74dc023499966ae879a88d
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2021-08-20 12:20:43 +0300

    after review + promised all

--- app.js
+++ app.js
@@ -1,72 +1,62 @@
-let fs = require('fs');
-let path = require('path');
-let util = require('util');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
 
-let firstFolder = path.join(__dirname, 'girls');
-let secondFolder = path.join(__dirname, 'boys');
+const firstFolder = path.join(__dirname, 'girls');
+const secondFolder = path.join(__dirname, 'boys');
 
-let boysFolder = path.join(__dirname, 'boys')
-let girlsFolder = path.join(__dirname, 'girls')
+const boysFolder = path.join(__dirname, 'boys')
+const girlsFolder = path.join(__dirname, 'girls')
 
 
 const renameFSPromise = util.promisify(fs.rename)
+const readdirFSPromise = util.promisify(fs.readdir)
+const readFilePromise = util.promisify(fs.readFile);
 
-
-console.log(__dirname);
-console.log(__filename);
-console.log(firstFolder)
 
 let sortBoysGirls = function (aimFolder) {
 
-    fs.readdir(aimFolder, (err, files) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
+    readdirFSPromise(aimFolder).then((files) => {
 
-        console.log(files)
-
+        console.log(`Current golder = ${aimFolder} 
+        files in folder: ${files}
+        `)
+        // start files brut
         files.forEach((file) => {
-            console.log(file);
             let filePath = path.join(aimFolder, file);
-            console.log(filePath);
-            fs.readFile(filePath, (err, data) => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
+            console.log(`${file} in folder ${filePath}`);
 
-                console.log(data.toString());
-                let dataText =  data.toString();
+            readFilePromise(filePath).then((data) => {
+
+                let dataText = data.toString();
+                console.log(dataText);
+
                 if (dataText.indexOf('female') === -1) {
                     console.log('this is boy')
                     console.log('current file loc = ' + filePath)
                     let newDest = path.join(boysFolder, file)
                     console.log('dest file loc = ' + newDest)
-                    renameFSPromise( filePath, newDest).catch( reason =>    {
-                        console.log(reason)
-                    } )
+
+                    renameFSPromise(filePath, newDest).then(() => {
+                        console.log('rename done, boy');
+
+                    })
                 } else {
                     console.log('this is girl')
                     let newDest = path.join(girlsFolder, file)
                     console.log('dest file loc = ' + newDest)
-                    renameFSPromise( filePath, newDest).catch( reason =>    {
-                        console.log(reason)
-                    } )
+                    renameFSPromise(filePath, newDest).then(() => {
+                        console.log('rename done, girl')
+                    })
                 }
-
             });
         })
+        // end files brut
+        console.log('end of function')
     })
-
-
 }
 
 sortBoysGirls(firstFolder);
 sortBoysGirls(secondFolder);
 
-
-
-console.log('dirname = '+ __dirname);
-console.log('filename = '+ __filename);
 
